4c97729499d7d1978440610fcdeb2845f21e2d3c
RG <r.goina2002@gmail.com>
2026-06-27 23:56:15 +0300

    Add typescript in conform

--- nvim/lua/theprimeagen/lazy/conform.lua
+++ nvim/lua/theprimeagen/lazy/conform.lua
@@ -5,12 +5,13 @@ return {
             formatters_by_ft = {
                 vue        = { "prettier" },
                 javascript = { "prettier" },
+                typescript = { "prettier" },
                 json       = { "prettier" },
             },
             format_on_save = function(bufnr)
                 local ft = vim.bo[bufnr].filetype
 
-                if vim.tbl_contains({ "vue", "javascript", "json" }, ft) then
+                if vim.tbl_contains({ "vue", "javascript", "json", "typescript" }, ft) then
                     return {
                         timeout_ms = 500,
                     }
