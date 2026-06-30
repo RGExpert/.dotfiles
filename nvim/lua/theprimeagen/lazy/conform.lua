473875ce323fb73c9d3489d90678b4a86a6cc40a
RG_Debian <r.goina@yahoo.com>
2026-06-30 12:50:51 +0300

    Increase confort timeout

--- nvim/lua/theprimeagen/lazy/conform.lua
+++ nvim/lua/theprimeagen/lazy/conform.lua
@@ -13,7 +13,7 @@ return {
 
                 if vim.tbl_contains({ "vue", "javascript", "json", "typescript" }, ft) then
                     return {
-                        timeout_ms = 500,
+                        timeout_ms = 2000,
                     }
                 end
             end
